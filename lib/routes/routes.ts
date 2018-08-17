728cee76f75a68a0caf85aa0f17ef40cba793889
dk <david.edews@gmail.com>
2018-08-17 01:53:26 -0300

    add login route

--- lib/routes/routes.ts
+++ lib/routes/routes.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction, Application } from 'express';
+import * as jwt from 'jsonwebtoken';
 import { User } from './../models/user-model';
+import { config } from './../../config/config';
 
 export class Routes {
   
@@ -11,16 +13,23 @@ export class Routes {
     app.post('/login', (req: Request, res: Response) => {
       const email = req.body.email;
       const password = req.body.password;
-      console.log(`email: ${email}, password: ${password}`); // $$$$dddd
-      User.find(
+      // console.log(`email: ${email}, password: ${password}`); // $$$$dddd
+      User.findOne(
         {email: email}, "name displayName email photoURL role",
         (err, user) => {
           if (err) {
             return res.status(500).send({message: err.message});
           }
           if (user) {
-            res.send(user);
+            // console.log("user", user); // $$$$dddd
+            const token = jwt.sign({role: user.role}, config.secret, {
+              expiresIn: "7d",
+              subject: user._id.toString()
+            });
+            return res.send({success: true, token: token, profile: user});
           }
+
+          res.send({success: false, message: "user-not-found"})
         });
     });
     
