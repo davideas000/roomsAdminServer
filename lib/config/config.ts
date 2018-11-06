b1301d5693ea8bf1e3888c3a9fc3e48797d798f2
david endrew <davideas000@gmail.com>
2018-11-06 12:37:05 -0300

    use MONGODB_URI env variable when present

--- lib/config/config.ts
+++ lib/config/config.ts
@@ -4,5 +4,5 @@ interface Config {
 
 export const config = {
   secret: "2c0f489e3a0db99b3c0446f3a812f48b29d0b41631d93a0a6108fb14e7361f40",
-  mongoURL: "mongodb://dev:dddddddd@localhost:27017/roomsAdmin"
+  mongoURL: process.env.MONGODB_URI || "mongodb://dev:dev@localhost:27017/dev"
 };
