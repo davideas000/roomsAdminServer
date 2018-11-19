bb2e6f783044c5dea1b92becfff462c5c817aa15
david endrew <davideas000@gmail.com>
2018-11-19 17:40:01 -0300

    mv login logic to user controller

--- lib/controllers/user.controller.ts
+++ lib/controllers/user.controller.ts
@@ -1,8 +1,74 @@
 import { Request, Response } from "express";
 import { UserModel } from "../models/user.model";
+import { config } from './../config/config';
+import * as jwt from 'jsonwebtoken';
+import * as expressJwt from 'express-jwt';
 
 export class UserController {
   
+  private _authGuard = expressJwt({
+    secret: config.secret
+  });
+
+  get authGuard() {
+    return this._authGuard;
+  }
+
+  login(req: Request, res: Response) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email)
+    {
+      return res.status(401).send({success: false, message: 'missing-email'});
+    }
+
+    if(!password) {
+      return res.status(401).send({success: false, message: 'wrong-password'});
+    }
+
+    UserModel.findOne(
+      {email: email},
+      (err, user) => {
+        if (err) {
+          return res.status(500).send({success: false, message: err.message});
+        }
+        if (!user) {
+          return res.status(401).send({success: false, message: 'user-not-found'})
+        }
+
+        user.checkPassword(password, function(result) {
+          if (result === true) {
+            const EXPIRES_IN = 600000;
+            const tokenBody = {expiresIn: EXPIRES_IN, subject: user._id.toString()};
+            // if (user.type === 'responsible') {
+            //   tokenBody.dep = user.
+            // }
+            const token = jwt.sign({role: user.role}, config.secret, tokenBody);
+
+            const tempUser = {
+              _id: user._id,
+              name: user.name,
+              displayName: user.displayName,
+              email: user.email,
+              photoURL: user.photoURL,
+              role: user.role
+            };
+            return res.send({success: true, token: token, profile: tempUser, expiresIn: EXPIRES_IN});
+          }
+
+          if (result === false) {
+            return res.status(401).send({success: false, message: 'wrong-password'})
+          }
+
+          if (result.message) {
+
+            res.status(500).send({success: false, message: err.message})
+          }
+        });
+      });
+  }
+
   getCurrentUser(req: Request, res: Response) {
     UserModel.findById((req as any).user.sub, "name displayName email photoURL role", (err, user) => {
       if (err) {
--- lib/routes/routes.ts
+++ lib/routes/routes.ts
@@ -1,9 +1,6 @@
 import { Request, Response, NextFunction, Application } from 'express';
-import * as jwt from 'jsonwebtoken';
-import * as expressJwt from 'express-jwt';
 import { body } from 'express-validator/check';
 
-import { config } from './../config/config';
 import { UserModel } from './../models/user.model';
 import { ReservationModel } from '../models/reservation.model';
 
@@ -23,91 +20,35 @@ export class Routes {
   
   routes(app: Application): void {
     
-    const authGuard = expressJwt({
-      secret: config.secret
-    });
-    
     app.get('/', (req: Request, res: Response) => {
       res.send({message: 'Api endpoint'});
     });
 
-    app.post('/login', (req: Request, res: Response) => {
-      const email = req.body.email;
-      const password = req.body.password;
-
-      if (!email)
-      {
-        return res.status(401).send({success: false, message: 'missing-email'});
-      }
-      
-      if(!password) {
-        return res.status(401).send({success: false, message: 'wrong-password'});
-      }
-      
-      UserModel.findOne(
-        {email: email},
-        (err, user) => {
-          if (err) {
-            return res.status(500).send({success: false, message: err.message});
-          }
-          if (!user) {
-            return res.status(401).send({success: false, message: 'user-not-found'})
-          }
-
-          user.checkPassword(password, function(result) {
-            if (result === true) {
-              const EXPIRES_IN = 600000;
-              const token = jwt.sign({role: user.role}, config.secret, {
-                expiresIn: EXPIRES_IN,
-                subject: user._id.toString()
-              });
-              
-              const tempUser = {
-                _id: user._id,
-                name: user.name,
-                displayName: user.displayName,
-                email: user.email,
-                photoURL: user.photoURL,
-                role: user.role
-              };
-              return res.send({success: true, token: token, profile: tempUser, expiresIn: EXPIRES_IN});
-            }
-
-            if (result === false) {
-              return res.status(401).send({success: false, message: 'wrong-password'})
-            }
-            
-            if (result.message) {
-
-              res.status(500).send({success: false, message: err.message})
-            }
-          });
-        });
-    });
+    app.post('/login', this.userController.login);
     
     app.route('/reservations')
-      .get(authGuard, this.reservationController.getReservations);
+      .get(this.userController.authGuard, this.reservationController.getReservations);
 
     app.post(
       '/reservation',
-      authGuard,
+      this.userController.authGuard,
       this.reservationController.validateNew(),
       this.reservationController.newReservation);
     
     app.route('/reservation/:id')
-      .put(authGuard, body('reason').optional().escape().trim(),
+      .put(this.userController.authGuard, body('reason').optional().escape().trim(),
            this.reservationController.validateUpdate,
            this.reservationController.updateReservation)
-      .delete(authGuard, this.reservationController.deleteReservation);
+      .delete(this.userController.authGuard, this.reservationController.deleteReservation);
 
-    app.get('/notifications', authGuard, this.notificationController.getCurrentUserNotifications);
-    app.put('/notifim', authGuard, this.notificationController.markNotificationsAsRead);
+    app.get('/notifications', this.userController.authGuard, this.notificationController.getCurrentUserNotifications);
+    app.put('/notifim', this.userController.authGuard, this.notificationController.markNotificationsAsRead);
 
-    app.get('/profile', authGuard, this.userController.getCurrentUser);
+    app.get('/profile', this.userController.authGuard, this.userController.getCurrentUser);
 
-    app.get('/rtypes', authGuard, this.roomController.getTypes);
+    app.get('/rtypes', this.userController.authGuard, this.roomController.getTypes);
 
-    app.get('/departments', authGuard, this.departmentController.getDeps);
+    app.get('/departments', this.userController.authGuard, this.departmentController.getDeps);
     
     // FIXME: english grammar
     // recives the following values as query paramenters:
@@ -117,7 +58,7 @@ export class Routes {
     // but all of them must be specified or none at all,
     // width, length, capacity, type and department are also
     // optional (any of them can be specified).
-    app.get('/rsearch', authGuard,
+    app.get('/rsearch', this.userController.authGuard,
             this.roomController.getExcludes,
             this.roomController.findRoomsAndExclude);
     
