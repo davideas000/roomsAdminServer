ebbf3ef626c9ffb5fcc9d77f1de2837317b3e21b
david endrew <davideas000@gmail.com>
2018-11-29 12:24:07 -0300

    refac(UserCon): assign req.user to a temp var
    this is just to make it easier to use.

--- lib/controllers/user.controller.ts
+++ lib/controllers/user.controller.ts
@@ -81,7 +81,8 @@ export class UserController {
   }
 
   getCurrentUser(req: Request, res: Response) {
-    UserModel.findById((req as any).user.sub, "name displayName email photoURL role", (err, user) => {
+    let user = (req as any).user;
+    UserModel.findById(user.sub, "name displayName email photoURL role", (err, user) => {
       if (err) {
         return res.status(500).send({message: err.message});
       }
